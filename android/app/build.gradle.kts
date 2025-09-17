0dcd896f6951195c098d26442c07348233c2efd9
Matin <matin7najafi@gmail.com>
2025-09-17 20:32:33 +0330

    Update NDK version to 27.0.12077973

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.apple_shop_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // <-- This line was changed
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
