40fb522167efbe4705a30d34557f6a554e41990a
Matin <matin7najafi@gmail.com>
2025-09-26 17:37:51 +0330

    Add automatic slides to banners

--- lib/widgets/banner_slider.dart
+++ lib/widgets/banner_slider.dart
@@ -1,40 +1,60 @@
 import 'package:apple_shop_app/constants/custom_colors.dart';
 import 'package:apple_shop_app/data/model/banner.dart';
 import 'package:apple_shop_app/widgets/cached_image.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
-// ignore: must_be_immutable
-class BannerSlider extends StatelessWidget {
-  List<BannerImage> bannerList;
-  BannerSlider({super.key, required this.bannerList});
+class BannerSlider extends StatefulWidget {
+  final List<BannerImage> bannerList;
+  const BannerSlider({super.key, required this.bannerList});
+
+  @override
+  State<BannerSlider> createState() => _BannerSliderState();
+}
+
+class _BannerSliderState extends State<BannerSlider> {
+  final CarouselSliderController _carouselController =
+      CarouselSliderController();
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
-    final PageController controller = PageController(viewportFraction: 0.9);
-
     return Stack(
       alignment: Alignment.bottomCenter,
       children: [
-        SizedBox(
-          height: 177,
-          child: PageView.builder(
-            itemCount: bannerList.length,
-            controller: controller,
-            itemBuilder: (context, index) {
-              return Container(
-                margin: EdgeInsets.symmetric(horizontal: 6),
+        CarouselSlider.builder(
+          carouselController: _carouselController,
+          itemCount: widget.bannerList.length,
+          itemBuilder:
+              (context, index, realIndex) => SizedBox(
+                height: 177,
+                width: double.infinity,
                 child: CachedImage(
-                  imageUrl: bannerList[index].thumbnail,
+                  imageUrl: widget.bannerList[index].thumbnail,
                   radius: 15,
                 ),
-              );
+              ),
+          options: CarouselOptions(
+            height: 177,
+            autoPlay: true,
+            enlargeCenterPage: true,
+            viewportFraction: 0.85,
+            aspectRatio: 16 / 9,
+            autoPlayCurve: Curves.easeInOut,
+            autoPlayAnimationDuration: const Duration(milliseconds: 1200),
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
             },
           ),
         ),
         Positioned(
           bottom: 10,
-          child: SmoothPageIndicator(
+          child: AnimatedSmoothIndicator(
+            activeIndex: _currentIndex,
+            count: widget.bannerList.length,
             effect: const ExpandingDotsEffect(
               expansionFactor: 4,
               dotHeight: 6,
@@ -42,8 +62,9 @@ class BannerSlider extends StatelessWidget {
               activeDotColor: CustomColors.blueIndicator,
               dotColor: Colors.white,
             ),
-            controller: controller,
-            count: bannerList.length,
+            onDotClicked: (index) {
+              _carouselController.animateToPage(index);
+            },
           ),
         ),
       ],
